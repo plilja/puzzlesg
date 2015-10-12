c604c7d29a6985373b449f3b51abd7a4b6a5bf69
patrik <patlil@kth.se>
2015-10-12 10:53:06 +0200

    mosquito

---
+++ mosquito/mosquito.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int M, P, L, E, R, S, N;
+    while (7 == scanf("%d %d %d %d %d %d %d", &M, &P, &L, &E, &R, &S, &N)) {
+        for (int i = 0; i < N; ++i)
+        {
+            int new_mosquitos = P / S;
+            P = L / R;
+            L = M * E;
+            M = new_mosquitos;
+        }
+        printf("%d\n", M);
+    }
+}
