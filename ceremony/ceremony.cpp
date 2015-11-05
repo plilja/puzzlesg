8dedc907fdabcc86ba26810dab7919bd41409427
patrik <patlil@kth.se>
2015-11-05 20:39:03 +0100

    cleaner solution

--- ceremony/ceremony.cpp
+++ ceremony/ceremony.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-const int HIGHEST = 1000000;
-
 int main()
 {
     int n;
@@ -15,28 +13,12 @@ int main()
         scanf("%d", &buildings[i]);
     }
     sort(buildings.begin(), buildings.end());
-    vector<int> heights(HIGHEST + 1, 0);
-    int curr = 0;
-    int j = 1;
-    for (int i = 0; i < n; ++i) {
-        if (i < n - 1 && buildings[i] == buildings[i + 1]) {
-            continue;
-        }
-        curr = buildings[i];
-        while (j < curr) {
-            heights[j] = heights[j - 1];
-            j++;
-        }
-        heights[j] = i + 1;
-        j++;
-    }
-    int r = HIGHEST + 1;
-    for (int i = 0; i <= HIGHEST; ++i) {
-        int num_heigher_than_i = n - heights[i];
-        int c = i + num_heigher_than_i;
-        if (c < r) {
-            r = c;
-        }
+    int r = buildings.size();
+    for (int i = 0; i < (int)buildings.size(); ++i) {
+        int horizontal_booms = buildings[i];
+        int vertical_booms = buildings.size() - i - 1;
+        r = min(r, horizontal_booms + vertical_booms);
+
     }
     printf("%d\n", r);
 }
