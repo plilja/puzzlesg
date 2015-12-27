71e8eb548c5636462f03c7a031f8dfa58da2a800
patrik <patlil@kth.se>
2015-12-27 19:36:38 +0100

    project restructure

---
+++ cpp/rationalsequence/rationalsequence.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int p;
+    scanf("%d", &p);
+    for (int _ = 0; _ < p; ++_)
+    {
+        int k, p, q;
+        scanf("%d %d/%d", &k, &p, &q);
+
+    }
+}
