8b715f178bc9d9310107526461a56a9b44445aa0
patrik <patlil@kth.se>
2016-08-01 23:07:12 +0200

    prettygoodcuberoot

---
+++ py3/prettygoodcuberoot/prettygoodcuberoot.py
@@ -0,0 +1,24 @@
+from math import *
+from decimal import Decimal
+import sys
+
+
+def solve(i, a, b):
+    m = (a + b) >> 1
+    r1 = m ** 3
+    r2 = (m + 1) ** 3
+    if r1 <= i and r2 >= i:
+        if abs(r1 - i) < abs(r2 - i):
+            return m
+        else:
+            return m + 1
+    if r1 > i:
+        return solve(i, a, m)
+    else:
+        return solve(i, m, b)
+
+
+for s in sys.stdin.readlines():
+    i = int(s)
+    r = solve(i, 1, 10 * int(Decimal(i) ** Decimal(1/3)))
+    print(r)
