0f366de3a789fd643963054fd517ff1e67862223
plilja <patlil@kth.se>
2016-11-06 13:45:52 +0100

    Add simple test runner

---
+++ cpp/run_all_tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+
+def test(f):
+    p = os.getcwd()
+    os.chdir(f)
+    cpps = list(filter(lambda f: f.endswith('.cpp'), os.listdir('.')))
+    print(f)
+    if cpps:
+        compile_command = 'clang++ -O2 -std=c++11 -o %s ' % f
+        os.system(compile_command + ' '.join(cpps))
+        os.system('~/workspace/algolib/util/checksol.py %s' % f)
+        os.remove(f)
+    os.chdir(p)
+
+def run():
+    print(os.getcwd())
+    for f in os.listdir('.'):
+        if os.path.isdir(f):
+            test(f)
+
+if __name__ == '__main__':
+    run()
