530ce4691f7db17c4af2b419362a498f14573389
plilja <patlil@kth.se>
2017-04-03 21:17:21 +0200

    Optimize performance

--- cpp/towers/towers.cpp
+++ cpp/towers/towers.cpp
@@ -3,86 +3,75 @@
 #include <algorithm>
 #include <memory>
 #include <cmath>
+#include <limits>
+#include <cassert>
+#include <cstring>
 
 using namespace std;
 
 double EPS = 10e-12;
+double INF = numeric_limits<double>::infinity();
 
-int get_or_default(const vector<int> &v, int idx, int def)
-{
-    if (idx < (int)v.size()) {
-        return v[idx];
+int get_or_default(const shared_ptr<vector<int>> &v, int v_end, int idx, int def) {
+    if (idx < v_end) {
+        return (*v)[idx];
     } else {
         return def;
     }
 }
 
-vector<int> break_at_first_one(const shared_ptr<vector<int>> &v)
-{
+int first_one(const shared_ptr<vector<int>> &v) {
     vector<int> res;
-    for (int a : *v) {
-        if (a == 1)
-            break;
-        res.push_back(a);
+    for (int i = 0; i < v->size(); ++i) {
+        if ((*v)[i] == 1) {
+            return i;
+        }
     }
-    return res;
+    return v->size();
 }
 
-bool less_than(const shared_ptr<vector<int>> &v1, const shared_ptr<vector<int>> &v2)
-{
-    vector<int> clean_v1 = break_at_first_one(v1);
-    vector<int> clean_v2 = break_at_first_one(v2);
-    bool rev = clean_v1.size() > clean_v2.size(); 
-
+double quotient(const shared_ptr<vector<int>> &v1, int v1_end, const shared_ptr<vector<int>> &v2, int v2_end) {
+    assert(v1_end <= v2_end);
     double ypow = 1;
     double d = 1;
-    int start = max(((int)clean_v1.size()) - 1, ((int)clean_v2.size()) - 1);
+    int start = v2_end - 1;
     for (int i = start; i >= 0; --i) {
-        int x = get_or_default(rev ? clean_v2 : clean_v1, i, 1); // use the vector with the smallest number of exponents for x
-        int y = get_or_default(rev ? clean_v1 : clean_v2, i, 1);
-        double log_x = log(x);
-        double log_y = log(y);
-        double fact = (log_x / log_y) * d - 1;
-
+        double x = get_or_default(v1, v1_end, i, 1); // use the vector with the smallest number of exponents for x
+        double y = get_or_default(v2, v2_end, i, 1);
+        double fact = d * log(x) / log(y) - 1;
         if (abs(fact) < EPS) {
             d = 1;
         } else {
-            d = powl(y, ypow * fact);
+            d = pow(y, ypow * fact);
         }
         if (d > 50) {
             // with the limits given in the problem statement, if we reach here d can never turn around and become less than 1
-            if (rev) {
-                return true;
-            } else {
-                return false;
-            }
-
+            return INF;
         }
         if (d < 0.05) {
             // with the limits given in the problem statement, if we reach here d can never turn around and become greater than 1
-            if (rev) {
-                return false;
-            } else {
-                return true;
-            }
-
+            return 0;
         }
         ypow = pow(y, ypow);
     }
-    if (rev) {
-        return d > 1 + EPS;
+    return d;
+}
+
+bool less_than(const shared_ptr<vector<int>> &v1, const shared_ptr<vector<int>> &v2) {
+    int v1_one = first_one(v1);
+    int v2_one = first_one(v2);
+    if (v1_one < v2_one) {
+        return quotient(v1, v1_one, v2, v2_one) < 1 - EPS;
     } else {
-        return d < 1 - EPS;
+        return quotient(v2, v2_one, v1, v1_one) > 1 + EPS;
     }
 }
 
-void solve(vector<shared_ptr<vector<int>>> &nums)
-{
+void solve(vector<shared_ptr<vector<int>>> &nums) {
     stable_sort(nums.begin(), nums.end(), less_than);
 }
 
-int main()
-{
+int main() {
     int m;
     int c = 1;
     while (1 == scanf("%d\n", &m)) {
@@ -109,5 +98,3 @@ int main()
         c++;
     }
 }
-
-
