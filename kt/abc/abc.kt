64205effcc609e8417d8532166bc687d97efca8d
Patrik Lilja <lilja.patrik@gmail.com>
2018-03-03 10:06:03 +0100

    abc

---
+++ kt/abc/abc.kt
@@ -0,0 +1,16 @@
+class Abc {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val xs = readLine()!!
+                    .split(" ")
+                    .map { s -> Integer.parseInt(s) }
+                    .sorted()
+            val out = readLine()!!
+                    .map { c ->  xs[c.toInt() - 'A'.toInt()].toString()}
+                    .joinToString(" ")
+            println(out)
+        }
+    }
+}
+
